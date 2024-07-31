89aa559aeb31a7b671a85ea6e505716239358ed8
Mrszlv <miroszlav.popovics@gmail.com>
2024-08-01 01:34:37 +0300

    commit to main-2

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+    const URL = "https://pixabay.com/api/";
+    const API_KEY = "45091921-bafbe55c990439d7032dec8c8";
+    
+export async function searchGalleryQuery(query, page = 1) {
+    try {
+        const response = await axios.get(URL, {
+            params: {
+                key: API_KEY,
+                q: query,
+                image_type: 'photo',
+                orientation: 'horizontal',
+                safesearch: true,
+                page: page,
+                per_page: 15,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching images:", error);
+        throw error;
+    }
+}
